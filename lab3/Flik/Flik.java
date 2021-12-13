e6f37df468b5a87daecc879342be5e8b56ca4e12
lingqu <2784247628@qq.com>
2021-12-13 21:43:10 +0800

    完成 lab2

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
-        return a == b;
+        //return a==b;
+        return a.equals(b);
+    }
+
+    @Test
+    public void testIsSameNumber(){
+        Integer a ;
+        Integer b;
+        a = 128;
+        b = 128;
+        assertTrue(isSameNumber(a,b));
     }
 }
